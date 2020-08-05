c04763f4fb6684e1105b6ebbb7d936ee5d3f36e7
Ebriopes <Antonio.vargasrosales@gmail.com>
2020-08-04 20:26:57 -0500

    test & cors added

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,9 @@ const ticket	= require('./ticketRoute')
 const user		= require('./userRoute');
 const router	= express.Router();
 
+router.get('/', (x,res)=> res.status(200).send({message: "URI base fine"}));
 router.use(user);
 router.use(ticket);
-router.get('/', (x,res)=> res.status(200).send({message: "URI base fine"}));
 
 
 module.exports	= router;
